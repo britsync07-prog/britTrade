3e07f25cc1e97199a75d69b685f50a90d78efeda
Saimon <mehedy303@gmail.com>
2026-04-13 03:12:30 +0600

    3rd

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,7 @@ const api = axios.create({
   },
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: any) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
