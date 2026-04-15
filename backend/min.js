10cef54d2da49185e18aac9d7f7a78c247caca6e
Saimon <mehedy303@gmail.com>
2026-04-15 20:44:20 +0600

    fixing nested repo and merging changes

--- backend/min.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3005;
-app.listen(PORT, () => {
-  console.log('Minimal server listening on ' + PORT);
-});
-console.log('Script execution reached end');
